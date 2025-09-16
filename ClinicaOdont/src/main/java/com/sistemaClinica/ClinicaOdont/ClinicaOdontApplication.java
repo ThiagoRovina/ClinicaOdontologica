e46a1cd6a5b306a785a2dd8dc00fcc6e892405f9
Thiago Rovina <ra124257@uem.br>
2025-09-16 09:39:45 -0300

    iniciando 2

--- ClinicaOdont/src/main/java/com/sistemaClinica/ClinicaOdont/ClinicaOdontApplication.java
+++ ClinicaOdont/src/main/java/com/sistemaClinica/ClinicaOdont/ClinicaOdontApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicaOdontApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CinicaOdontApplication.class, args);
+		SpringApplication.run(ClinicaOdontApplication.class, args);
 	}
 
 }
