9a203d9c602fbf6a4edb5b78819dbb965628ed5d
Thiago Rodrigues Rovina <110912255+ThiagoRovina@users.noreply.github.com>
2025-09-16 09:00:48 -0300

    Add files via upload

---
+++ CinicaOdont/src/main/java/com/sistemaClinca/CinicaOdont/CinicaOdontApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaClinca.CinicaOdont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinicaOdontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinicaOdontApplication.class, args);
+	}
+
+}
