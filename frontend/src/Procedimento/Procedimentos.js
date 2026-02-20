6f6e8b1ea9d3e99b972363f8b7f41fd411fe65fd
ThiagoRovina <ra124257@uem.br>
2026-02-20 09:49:16 -0300

    Adicionando melhorias

---
+++ frontend/src/Procedimento/Procedimentos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Procedimentos() {
+    return (
+        <div>
+            <h1>Procedimentos</h1>
+        </div>
+    );
+}
+
+export default Procedimentos;
