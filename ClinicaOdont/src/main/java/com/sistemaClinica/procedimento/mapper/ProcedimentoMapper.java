0b8114c9e2a1483e4334b83e418679212f5f7781
ThiagoRovina <ra124257@uem.br>
2026-03-20 11:09:10 -0300

    - Ignore

--- ClinicaOdont/src/main/java/com/sistemaClinica/procedimento/mapper/ProcedimentoMapper.java
+++ ClinicaOdont/src/main/java/com/sistemaClinica/procedimento/mapper/ProcedimentoMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.factory.Mappers;
 import com.sistemaClinica.procedimento.dto.ProcedimentoDTO;
 import com.sistemaClinica.procedimento.model.Procedimento;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "default")
 public interface ProcedimentoMapper {
     ProcedimentoMapper INSTANCE = Mappers.getMapper(ProcedimentoMapper.class);
 
